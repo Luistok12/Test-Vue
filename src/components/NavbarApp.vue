a5a867d45731cbc4bfd19f4cab5dc72d6625a6a7
Luis Amadeo Tolaba Cari <cabj_luis93@hotmail.com>
2023-08-25 17:29:31 -0300

    Algo de responsive, menus y submenus, aun queda pulir

--- src/components/NavbarApp.vue
+++ src/components/NavbarApp.vue
@@ -5,9 +5,17 @@
             <span></span>
             <span></span>
         </div>
+        <!-- Este div es solo para realizar el icono del menu, podemos usar un img y saltar esta parte -->
         <ul class="menu" :class="{ 'open': isMenuOpen }">
             <li><router-link to="/" class="link">Inicio</router-link></li>
-            <li><router-link to="/categorias" class="link">Categorias</router-link></li>
+            <li class="categorias"><router-link to="/categorias" class="link">Categorias</router-link>
+                <ul class="submenu">
+                    <li><router-link to="/electrodomesticos" class="link">Electrodomesticos</router-link></li>
+                    <li><router-link to="/comestibles" class="link">Comestibles</router-link></li>
+                    <li><router-link to="/bebidas" class="link">Bebidas</router-link></li>
+                    <li><router-link to="/golosinas" class="link">Golosinas</router-link></li>
+                </ul>
+            </li>
             <li><router-link to="/sucursales" class="link">Sucursales</router-link></li>
             <li><router-link to="/acerca" class="link">Acerca de nosotros</router-link></li>
             <li><router-link to="/galeria" class="link">Galeria</router-link></li>
@@ -47,18 +55,21 @@ nav {
         padding: 10px;
         background-color: $bg-200;
         cursor: pointer;
+        border-radius: 10px;
         top: 20px;
         right: 20px;
         position: absolute;
-        border-radius: 10px;
 
+        // Las 3 ultimas lineas es para acomodar el menu-icono, se la hace asi generalmente
         span {
             width: 30px;
             height: 4px;
             background-color: $text-100;
             margin: 3px 0;
+            transition: all 0.3s ease-in-out
         }
     }
+
     // Todo bien hasta aca, ningun estilo puesto sin sentido
     .menu {
         display: flex;
@@ -78,9 +89,21 @@ nav {
             &:hover {
                 background-color: $bg-300;
 
+                .submenu {
+                    display: block;
+                    position: absolute;
+                    list-style: none;
+                    margin-top: 10px;
+                    margin-left: -0.6%;
+                }
+
                 .link {
                     color: $text-100;
                 }
+            }
+
+            .submenu {
+                display: none;
             }
 
             .link {
@@ -88,6 +111,10 @@ nav {
                 text-decoration: none;
             }
         }
+        // Doy ancho al contenedor de Categorias
+        .categorias {
+                width: 150px;
+            }
     }
 
     @media (max-width: $breakpoint-tablet) {
@@ -98,25 +125,51 @@ nav {
         .menu {
             display: none; // Oculta la lista de enlaces en pantallas pequeñas
             flex-direction: column;
-            align-items: center;
-            // position: absolute;
-            top: 60px;
-            left: 0;
-            background-color: #f9f9f9;
-            width: 100%;
-            padding: 10px;
-            box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
-            z-index: 1;
 
+
+            // align-items: center;
+            // position: absolute;
+            // top: 60px;
+            // left: 0;
+            // background-color: #f9f9f9;
+            // width: 100%;
+            // padding: 10px;
+            // box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+            // z-index: 1;          //Para que en una superposicion, esta sea "prioridad"
+            // position absolute y z-index deben usarse al mismo tiempo
             &.open {
-                display: flex; // Muestra la lista de enlaces cuando el menú está abierto
+                display: block; // Muestra la lista de enlaces cuando el menú está abierto
+
+                li {
+                    animation-name: fadeInRight;
+                    animation-duration: 500ms;
+
+                    .submenu {
+                        display: none;
+                    }
+                }
+
+                // Aca se "invoca" la animacion
             }
 
             li {
-                margin-right: 0;
-                margin-bottom: 10px;
+                // margin-right: 0;
+                // margin-bottom: 10px;
+                text-align: center;
             }
         }
+    }
+}
+
+@keyframes fadeInRight {
+    from {
+        opacity: 0;
+        transform: translate3d(100%, 0, 0);
+    }
+
+    to {
+        opacity: 1;
+        transform: none;
     }
 }
 </style>
